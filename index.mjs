6116abb2c690756c6f9325dbcfab790e4c387d33
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 19:02:01 -0400

    wrapping up server code

--- index.mjs
+++ index.mjs
@@ -3,3 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+
+//middleware
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`);
+});
