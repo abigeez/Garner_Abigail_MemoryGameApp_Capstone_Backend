1bc2f1c9726172f75de7fe6eb89fa02da2eaab04
Abi Garner <abigail.gee@hotmail.com>
2024-06-25 18:40:56 -0400

    comment section model done

--- models/CommentSection.js
+++ models/CommentSection.js
@@ -1,20 +1,13 @@
+
 const {Schema, model } = require('../db/conn');
 
-const commentSchema = new Schema(
-{
-    name: {
-        type: String,
-        required: true,
-        unique:true,
-    },
-    comment: {
-        type: String,
-        required: true,
+const commentSchema = new Schema({
+    author: String,
+    body: String,
+    comments :[{body: String, date: Date}],
+    date: {type: Date, default: Date.now}
 
-    },
-},
-    {
-        timestamps:true,
+    
     }
 
 
