4e56871c8bdaadcf31465d65bb628a7f93bea017
Abi Garner <abigail.gee@hotmail.com>
2024-06-25 19:17:54 -0400

    completed controllers file

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,5 +6,36 @@ async function index (req,res) {
         if (comments) {
             res.status(200).send(comments);
         }
+    } catch (err) {
+        res.status(400).send(err);
     }
 }
+
+async function create(req,res) {
+    try{
+        const createdComment = await Comments.create(req.body);
+        if (createdComment) {
+            res.status(201).send(createdComment);
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+
+
+async function destroy(req,res) {
+    try {
+        const deletedComment = await Comments.findByIdandDelete(req.params.id);
+        if (deletedComment) {
+            res.status(201).send(deletedComment);
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+module.exports = {
+    index,
+    create,
+    destroy,
+};
